19ead886051c1739fc6dbed20ee02727bfff263f
Fermin Silva <silvafermn@gmail.com>
2026-03-28 09:18:12 -0300

    Fix favicons

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'gymlog-v6';
+const CACHE_NAME = 'gymlog-v8';
 
 const PRECACHE_URLS = [
   '/',
@@ -8,8 +8,12 @@ const PRECACHE_URLS = [
   '/assets/css/rutina.css',
   '/assets/js/rutina-app.js',
   '/manifest.json',
-  '/assets/icons/icon-192.svg',
-  '/assets/icons/icon-512.svg',
+  '/assets/icons/favicon.ico',
+  '/assets/icons/favicon-16x16.png',
+  '/assets/icons/favicon-32x32.png',
+  '/assets/icons/apple-touch-icon.png',
+  '/assets/icons/android-chrome-192x192.png',
+  '/assets/icons/android-chrome-512x512.png',
   'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Playfair+Display:wght@500;700&display=swap'
 ];
 
